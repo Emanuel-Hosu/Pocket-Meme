78ae0bd128df7a31730a523e9d8321b93fec1722
Oscar Nzabarinda Mukeshimana <101839576+MainKataVerde@users.noreply.github.com>
2025-01-16 20:10:29 +0100

    tarea c23 terminada , añadimos metodo por si alguno de los userid ya existe generar uno nuevo

--- backend/cards/user_management.py
+++ backend/cards/user_management.py
@@ -25,12 +25,23 @@ def user_creation(name , password, mail):
     
     arrayLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     arrayNumbers = ['0','1','2','3','4','5','6','7','8','9']
+    
     for letter in range(4):
         if (random.randint(0,1) == 0):
             user_id += arrayNumbers[random.randint(0,9)] + arrayLetters[random.randint(0,25)]
         else:
             user_id += arrayLetters[random.randint(0,25)] + arrayNumbers[random.randint(0,9)]
     
+    '''
+    if the user_id already exists, generate a new one until it is unique
+    '''
+    if user_database.find_one({"_id": user_id}):
+        while user_database.find_one({"_id": user_id}): 
+            for letter in range(4):
+                if (random.randint(0,1) == 0):
+                    user_id += arrayNumbers[random.randint(0,9)] + arrayLetters[random.randint(0,25)]
+                else:
+                    user_id += arrayLetters[random.randint(0,25)] + arrayNumbers[random.randint(0,9)]
     '''
     create the new user
     '''
