86d9b3bfd09405a5f1ef79681c6c3dddae91d68d
Caye110905 <145046977+Caye110905@users.noreply.github.com>
2025-01-17 17:39:28 +0100

    Input de registrarse hecho

---
+++ src/components/RegistrarseInput.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const RegistrarseInput = () => {
+  return (
+    <button className="flex items-center gap-2 bg-green-600 hover:bg-green-700 shadow-md px-4 py-2 rounded-full text-sm text-stone-50 transition-colors">
+      <span>Registrarse</span>
+    </button>
+  );
+};
