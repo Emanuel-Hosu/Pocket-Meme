5b8bd5a9ca3cb8a0792710cd9067f054816dcfac
ArthurAlvvarez <arturo.alvarez.gomez@colegiolitterator.com>
2025-01-20 15:12:51 +0100

    cambio de color

--- src/components/Registrarse.jsx
+++ src/components/Registrarse.jsx
@@ -9,16 +9,16 @@ import { User } from 'lucide-react';
 
 export const Registrarse = () => {
     return (
-      <div className="bg-black h-screen w-screen px-10 py-20 flex items-center justify-center">
-      <div className=' bg-slate-300 p-2 border-2 border-black rounded-lg gap-4 flex flex-col items-center justify-center w-full h-full max-w-3xl'>
+      <div className="bg-gradient-to-b from-blue-400 via-blue-300 to-blue-200 h-screen w-screen px-10 py-20 flex items-center justify-center">
+      <div className=' bg-white/80 backdrop-blur-sm shadow-lg  rounded-lg gap-4 flex flex-col items-center justify-center w-full h-full max-w-3xl'>
         <img src="/logopkt.png" alt="Logo" className="w-32 h-32 mx-auto" />
         <UserInput />
         <CorreoInput />
         <PasswordInput />
         <RegistrarseInput />
-        <a className="text-blue-400 transition block mt-3">
+        <a className="text-sm text-center mt-4 text-gray-600">
         Si ya tienes una cuenta{" "}
-        <a className="text-blue-600" href="./components/IniciarSesion">
+        <a className="text-blue-600 hover:underline ml-1" href="./components/IniciarSesion">
           inicia sesión.{" "}
         </a>
         Y vuelve a tu aventura
