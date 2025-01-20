8782c825705d600bc670fd83dfb8fc7838a0fb38
david <davidespeton@gmail.com>
2025-01-20 10:32:11 +0100

    Inicio del componente HeaderMain

---
+++ src/components/HeaderMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HeaderMain = () => {
+    return (
+        <div className="flex justify-between items-center">
+            <button className="rounded-full">
+                <img className="size-40" src="/gatt.jpg" alt="pfpImage" />
+            </button>
+            <div></div>
+        </div>
+    );
+};
