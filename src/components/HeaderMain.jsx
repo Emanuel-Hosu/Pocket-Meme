99d5998de4cc8ae6baee2bd5b79617450963c7ac
david <davidespeton@gmail.com>
2025-01-20 15:41:52 +0100

    Creación barra de progeso de nivel

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const HeaderMain = () => {
+const PlayerLevelBar = () => {
+    const [progress, setProgress] = useState(70);
+    //para subir porcentage updateProgress(progress + 10)
+    //para bajar porcentage updateProgress(progress - 10)
+    const updateProgress = (newProgress) => {
+        if (newProgress >= 0 && newProgress <= 100) {
+            setProgress(newProgress);
+        }
+    };
+
     return (
-        <div className="flex justify-between items-center">
-            <button className="rounded-full">
-                <img className="size-40" src="/gatt.jpg" alt="pfpImage" />
-            </button>
-            <div></div>
+        <div className="flex justify-center items-center">
+            <div className="flex flex-col items-center bg-red-400 shadow-2xl rounded-lg">
+                <button className="py-2">
+                    <img className="shadow-lg rounded-full size-20" src="/public/gatt.jpg" alt="" />
+                </button>
+                <div className="pb-2 w-80">
+                    <div className="relative bg-gray-300 rounded-full h-6">
+                        <div
+                            className="top-0 left-0 absolute bg-green-500 rounded-full h-full transition-all duration-300"
+                            style={{ width: `${progress}%` }}
+                        ></div>
+                    </div>
+                    <div>
+                        <p>
+                            Nivel 7
+                        </p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
+
+export default PlayerLevelBar;
