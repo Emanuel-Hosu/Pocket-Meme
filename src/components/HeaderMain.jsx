276b1e39061a81eb7ce0c272f94ae6c43d160f80
david <davidespeton@gmail.com>
2025-01-29 19:02:20 +0100

    Completado headerMain

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,29 +1,40 @@
 import React, { useState } from "react";
 
-const PlayerLevelBar = () => {
-    const [progress, setProgress] = useState(60);
+const MainHeader = () => {
+    const [progress, setProgress] = useState(50);
     //para subir porcentage updateProgress(progress + 10)
-    //para bajar porcentage updateProgress(progress - 10)
+    //funcion para subir la barra de nivel
     const updateProgress = (newProgress) => {
-        if (newProgress >= 0 && newProgress <= 100) {
+        if (newProgress >= 0 && newProgress <= 20) {
             setProgress(newProgress);
+        }
+    };
+    //funcion para subir el nivel
+    const [lvl, setLvl] = useState(1);
+    const updateLvl = (newLvl) => {
+        if (newLvl >= 1 && newLvl <= 100) {
+            setLvl(newLvl);
         }
     };
 
     return (
         <div className="flex justify-center items-center w-full">
-            <div className="flex flex-col items-center bg-red-400 shadow-2xl rounded-lg min-w-96 max-w-96">
-                <button className="py-4">
-                    <img className="shadow-lg rounded-full size-20" src="/public/gatt.jpg" alt="" />
+            <div className="flex flex-col items-center bg-stone-200 shadow-xl mx-5 rounded-3xl w-full" >
+                <button className="shadow-md mt-4 rounded-full">
+                    <img className="shadow-md rounded-full size-20" src="/public/gatt.jpg" alt="" />
                 </button>
 
 
-                <div className="bg-green-700 py-2 rounded-lg w-full">
-                    <div className="bg-purple-600 rounded-full h-6">
+                <div className="my-4 pb-2 rounded-lg w-36">
+                    <div className="relative flex shadow-inner rounded-full h-6">
                         <div
-                            className="top-0 left-0 bg-green-400 rounded-full h-full transition-all duration-300"
+                            className="absolute bg-green-400 rounded-full h-full"
                             style={{ width: `${progress}%` }}
                         ></div>
+                        <div className="ml-40">
+                            Nvl.{lvl}
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -31,4 +42,4 @@ const PlayerLevelBar = () => {
     );
 };
 
-export default PlayerLevelBar;
+export default MainHeader;
