acdc24ac3b0e16ba4a9ea5ba0827db74577b767b
adriesco <144839808+adriesco@users.noreply.github.com>
2025-01-30 15:48:07 +0100

    c-59,76,77,78,79

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Registrarse } from "./components/Registrarse";
 import { IniciarSesion } from "./components/IniciarSesion";
-import {Logo} from './components/Logo';
+import { Logo } from "./components/Logo";
 import { BotonFormulario } from "./components/BotonFormulario";
 import { Formulario } from "./components/Formulario";
+import { UserId } from "./components/UserId";
 
 function App() {
   return (
     <>
-    <Formulario />
+      <UserId />
     </>
   );
 }
